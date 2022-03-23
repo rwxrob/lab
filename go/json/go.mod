6eb49787f0a5d2325c295af491e23c92ff67c189
rwxrob <rob@rwx.gg>
2022-03-23 03:34:03 -0400

    Capture how `inline` works with YAML/JSON

---
+++ go/json/go.mod
@@ -0,0 +1,7 @@
+module some/thing
+
+go 1.18
+
+require github.com/ghodss/yaml v1.0.0
+
+require gopkg.in/yaml.v2 v2.4.0 // indirect
--- go/json/main.go
+++ go/json/main.go
@@ -1,20 +1,38 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+
+	"gopkg.in/yaml.v2"
 )
 
+// this works
+type Foo struct {
+	One int            `yaml:"two"`
+	Two int            `yaml:"one"`
+	O   map[string]any `yaml:",inline"`
+}
+
 func main() {
-	buf := `
-ну <что> же
-😈💢©💖💘💥
-`
-	byt, err := json.Marshal(buf)
-	if err != nil {
+	/*
+			buf := `
+		ну <что> же
+		😈💢©💖💘💥
+		`
+			byt, err := json.Marshal(buf)
+			if err != nil {
+				log.Print(err)
+			}
+			fmt.Println(string(byt))
+	*/
+
+	foo := new(Foo)
+	buf, _ := os.ReadFile("foo.json")
+	if err := yaml.Unmarshal(buf, foo); err != nil {
 		log.Print(err)
 	}
-	fmt.Println(string(byt))
+	fmt.Println(foo)
 
 }
