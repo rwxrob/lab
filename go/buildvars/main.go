65ae2849cfb4722988288f50fcc2c89205c19cc2
rwxrob <rob@rwx.gg>
2022-03-23 09:33:24 -0400

    Show that -ldflags does not work with const

--- go/buildvars/main.go
+++ go/buildvars/main.go
@@ -2,11 +2,14 @@ package main
 
 import "fmt"
 
+const VersionConst = ``
+
 var Version string
 
 //var VersionInt int // nope
 
 func main() {
 	fmt.Println(Version)
+	fmt.Println(VersionConst)
 	// 	fmt.Println(VersionInt)  // nope
 }
