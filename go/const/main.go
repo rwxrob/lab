44abc52570373f51b3ecee9d3ed06992ae39d11e
rwxrob <rob@rwx.gg>
2022-04-08 08:08:37 -0400

    Add var/const func go aliases

--- go/const/main.go
+++ go/const/main.go
@@ -6,10 +6,15 @@ type Foo struct {
 	Some string
 }
 
+func Bar() { fmt.Println("bar") }
+
+var bbar = Bar
+
 // nope, constants cannot be structs
 // const MyFoo = Foo{"something"}
 // const MyFoo = struct{ Foo string }{"something"}
 
 func main() {
+	bbar()
 	fmt.Println("vim-go")
 }
